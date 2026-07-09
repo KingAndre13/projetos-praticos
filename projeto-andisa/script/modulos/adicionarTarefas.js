a3884a45dd594eca78e12e30d5a067f8954754dc
André Diogo <120501460+KingAndre13@users.noreply.github.com>
2026-07-08 22:30:44 -0300

    Completa implementação do módulo adicionarTarefa

--- projeto-andisa/script/modulos/adicionarTarefas.js
+++ projeto-andisa/script/modulos/adicionarTarefas.js
@@ -1,7 +1,8 @@
 export function adicionarTarefas () {
     let botaoAdicionar = document.querySelector('#opcaoAdicionar');
     let criarTarefas = document.querySelector('#criarTarefa');
-    let botaoTarefa = document.querySelector("#botaoTarefa");
+    let botaoTarefa = document.querySelector('#botaoTarefa');
+    let tarefasPrincipais = document.querySelector('#tarefasPrincipais');
 
 
     botaoAdicionar.addEventListener('click', () => {
@@ -10,6 +11,22 @@ export function adicionarTarefas () {
 
     botaoTarefa.addEventListener('click', () => {
         criarTarefas.style.display = 'none';
+
+        let tarefaCriada = document.createElement('p');
+
+        let tituloTarefa = document.querySelector('#titleTarefa');
+        let getTituloTarefa = tituloTarefa.value;
+
+        let horarioTarefa = document.querySelector('#horarioTarefa');
+        let getHorarioTarefa = horarioTarefa.value;
+
+        let dataTarefa = document.querySelector('#dataTarefa');
+        let getDataTarefa = dataTarefa.value;
+
+
+        tarefaCriada.innerHTML = `<strong>Tarefa: </strong> ${getTituloTarefa} - <strong>Horário: </strong>${getHorarioTarefa} - <strong>Data: </strong>${getDataTarefa}`;
+        tarefasPrincipais.appendChild(tarefaCriada);
+
     });
 
 
